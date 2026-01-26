ef2fcbf1ac31ceb1641fa757f34811e112f10a2d
Abdallah Maher <abdo17.maher@gmail.com>
2026-01-26 11:03:24 +0200

    Orgnizing of Tasks file names

---
+++ Myfunc_ex_file2.c
@@ -0,0 +1,7 @@
+void myNameFunc()
+{
+    char my_name[50]; //why result name is first name only?
+    printf("please enter your name:");
+    scanf("%s", &my_name);
+    printf("My name is : %s", my_name);
+}
